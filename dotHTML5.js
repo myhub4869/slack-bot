e32115d753ca5768caa3f739872a0ca4a40fc01b
kenta-miyahara <pt-kenta-miyahara@m.t8s.jp>
2018-01-23 10:54:04 +0900

    first commit

---
+++ dotHTML5.js
@@ -0,0 +1,32 @@
+const FeedParser	= require( 'feedparser' );
+const Request		= require( 'request' );
+const Utillity		= require( 'util' );
+const targetURL		= "https://dothtml5.com/feed.xml";
+
+var rssRequest	= Request( targetURL );
+var feedParser	= new FeedParser({});
+var rssItems	= [];
+var today		= new Date();
+
+rssRequest.on( 'response' , function ( responce ) {
+	this.pipe( feedParser );
+});
+
+feedParser.on( 'readable' , function() {
+	while( item = this.read() ) {
+		//console.log( JSON.stringify( item ) );
+		rssItems.push( item );
+	}
+});
+feedParser.on( 'end' , function() {
+	rssItems.forEach( function( item ) {
+		var date	= new Date( item.date );
+		console.log( Utillity.format( '【%s】[%d/%d/%d]' , item.title , date.getFullYear() , date.getMonth() + 1 , date.getDate() ) );
+
+		if( ( date.getFullYear()	=== today.getFullYear()	) &&
+			( date.getMonth()		=== today.getMonth()	) &&
+			( date.getDate()		=== today.getDate()		) ) {
+				console.log( Utillity.format( 'target link is : %s' , item.link ) );
+			}
+	});
+});
